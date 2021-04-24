49b192144f19bbfe19568a78df060ceb6a02d996
Moulham Hallak <mou990@gmail.com>
2021-04-24 00:27:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ document.addEventListener("keydown", function(e) {
 });
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
  
   if (left > 0) {
     dodger.style.left = `${left + 1}px`;
